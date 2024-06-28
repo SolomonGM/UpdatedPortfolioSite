4ff7f16975f0b6f86ea4f92d46c63da885b4f8a8
Solomon Oduyebo <bae82781@gmail.com>
2024-06-28 06:37:39 +0100

    Layout designed for splashscreen
    Additional changes to the layout has been made. New design and button to continue to homepage.

--- public/js/index.js
+++ public/js/index.js
@@ -103,3 +103,11 @@ if ('ontouchstart' in document.documentElement) {
 }
 
 document.addEventListener(interactionEvent, sparcle);
+
+let anims = [...document.querySelectorAll("[anim]")];
+console.log(anims);
+let click = (el, cb) => el.addEventListener("click", cb);
+let toggle = (el) => el.classList.toggle("toggled");
+let clickTog = (el) => click(el, () => toggle(el));
+anims.map(clickTog);
+
